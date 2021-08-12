c1c1a6a509c3b7966e5a4bb9e0c707c88c5175c1
Bence Sárkány <sarkany.bence@teralkoto.hu>
2021-08-12 19:29:17 +0200

    d3

--- src/com/company/Greeting.java
+++ src/com/company/Greeting.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Greeting {
     public static void greet(String name) {
-        System.out.println("Hello "+name+"!");
+        System.out.println("Greetings");
     }
 }
