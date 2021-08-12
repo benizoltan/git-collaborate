5ce2c2d29cc5fdc0509d5f44435604cd6c1c117c
eberhardt86 <peter.eberhardt@hotmail.hu>
2021-08-12 19:22:09 +0200

    c2

--- src/com/company/cheer.java
+++ src/com/company/cheer.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class cheer {
     
-    public cheer(){
-        System.out.println("s1");
+    public cheer(String text){
+        System.out.println("text");
     }
 }
