cb492adb9e078ed43e4e04d63d9779ee359a1bb2
marcellgyori <marcellgyori2@gmail.com>
2021-08-12 20:00:32 +0200

    b6

--- src/com/company/Greeting.java
+++ src/com/company/Greeting.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Greeting {
     public static void greet(String name) {
-        System.out.println("Greetings");
+        System.out.println("Greetings for the parameter");
     }
 }
