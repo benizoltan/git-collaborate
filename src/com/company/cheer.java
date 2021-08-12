6254820971c9a2d914ef4addd0ffcd295564cfe4
eberhardt86 <peter.eberhardt@hotmail.hu>
2021-08-12 19:49:50 +0200

    c5

--- src/com/company/cheer.java
+++ src/com/company/cheer.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class cheer {
     
-    public cheer(String text){
-        System.out.println("Brainfuck");
+    public cheer(String text, Integer howOften){
+        System.out.println(text + ": " + howOften + "times");
     }
 }
